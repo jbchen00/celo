2340f380aa8b6e52eca9f23146ec9fbe4d18b9ee
joebnc <joe@bravenewcoin.com>
2020-04-20 00:20:36 +1200

    added migration

--- RandomUserApi/Migrations/20200419121924_fix-fk4.Designer.cs
+++ RandomUserApi/Migrations/20200419121924_fix-fk4.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RandomUserApi.Data;
 
 namespace RandomUserApi.Migrations
 {
     [DbContext(typeof(MyDbContext))]
-    partial class MyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200419121924_fix-fk4")]
+    partial class fixfk4
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ RandomUserApi/Migrations/20200419121924_fix-fk4.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RandomUserApi.Migrations
+{
+    public partial class fixfk4 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ProfileImageEntities_UserId",
+                table: "ProfileImageEntities");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UserId",
+                table: "ProfileImageEntities",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProfileImageEntities_UserId",
+                table: "ProfileImageEntities",
+                column: "UserId",
+                unique: true,
+                filter: "[UserId] IS NOT NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ProfileImageEntities_UserId",
+                table: "ProfileImageEntities");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UserId",
+                table: "ProfileImageEntities",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProfileImageEntities_UserId",
+                table: "ProfileImageEntities",
+                column: "UserId",
+                unique: true);
+        }
+    }
+}
